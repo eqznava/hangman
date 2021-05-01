3fa6541b3891a5d253ed8d1843a44e79c452ff0e
Sergio Enriquez Nava <eqz.nava@gmail.com>
2021-05-01 18:38:36 -0500

    Test to compare function

--- hangman.py
+++ hangman.py
@@ -6,17 +6,28 @@ def read():
         for line in file:
             words.append(line)
         random_word = random.choice(words)
-    return random_word
+        return random_word
 
-
-def compare(random_word):
+#Convert to comprehension function
+def random_words_to_list(random_word):
     word_to_list = list(random_word)
-    print(word_to_list)
+    word_to_list.pop()
+    return word_to_list
+
+
+def compare(word_to_list, my_list_word):
+    if word_to_list == my_list_word:
+        print('\nYou did, !!! wow your a genius !!!')
+    else:
+        print('Sorry your wrong bad luck')
 
 
 def run():
-    print(read())
-    compare(read())
+    w = random_words_to_list(read())
+    print(w)
+    my_word = input('Intenta adivinar la palabra que estoy pensando:\n')
+    my_list_word = list(my_word)
+    compare(w, my_list_word)
     # print('---------------------------------------------------- H A N G M A N ----------------------------------------------------\n')
     # print('Hola (⌐■_■) tendras que adividar la palabra que estoy pensando ^_^ de lo contrario nuestro amigito morira ^_^\n')
     # print('\n')
