8258319bc43499bd643ac38d6daa0424f3b0d2d9
Sergio Enriquez Nava <eqz.nava@gmail.com>
2021-05-03 17:26:14 -0500

    Building more files and functions[com_lett, trasnform_characters]

--- hangman.py
+++ hangman.py
@@ -1,4 +1,9 @@
+import os
 import random
+
+def limpiar():
+    os.system('cls')
+
 
 def read():
     words = []
@@ -24,34 +29,45 @@ def transform_to_list():
 
 def transform_characters(lts=[]):
     all_index = []
-    for i in range(len(lts)):
-        all_index.append('_')
-    for i in all_index:
-        print(i, end = ' ')
+    with open('C:/Users/natif/Escritorio/papa/Python/python_intermedio/proyecto_final/archivos/rw_transform.txt','w', encoding='utf-8') as file:
+        for i in range(len(lts)):
+            all_index.append('_')
+        for i in all_index:
+            print(i, end = ' ')
+        underscore = ''.join(all_index)
+        file.write(underscore)
     
 
 def com_lett(my_letter):
     word_to_list = []
+    indices = []
+    words = []
     with open('C:/Users/natif/Escritorio/papa/Python/python_intermedio/proyecto_final/archivos/random_word.txt','r', encoding='utf-8') as file:
         for line in file:
             word_to_list = line
             wtl = list(word_to_list)
             wtl.pop()
-    for  lett in word_to_list:
-        if lett == my_letter:
-            print('La encontre !!!')
-        else:
-            print('No hay ninguna coinsidencia.')
+    indices = [indice for indice, lett in enumerate(wtl) if lett == my_letter]
+    with open('C:/Users/natif/Escritorio/papa/Python/python_intermedio/proyecto_final/archivos/rw_transform.txt','r', encoding='utf-8') as file:
+        for line in file:
+            words = line
+            w = list(words)
+        #Me falta tomar los indices almacenados en indices y pedir que esos mismos indices los remplace 
+        #por las letras que correspondan a dichos indices , para lo cual hay que hacer otro list comprehension 
+        #para sacar las letras que corresponden a los indices y asi remplazarlos en el archivo rw_transform.
+    print(w)
 
+#Esta pendiente 
+def run():    
+    # my_list=[1,2,3]
 
-def run():
-    # w = random_words_to_list(read())
-    print(read())
-    print('\n')
-    transform_characters(transform_to_list())
-    print('\n')
-    my_letter = input('Try to guess the letter i\'m thinking of:\n')
-    com_lett(my_letter)
+    # while not my_list:
+        print(read())
+        print('\n')
+        transform_characters(transform_to_list())
+        print('\n')
+        my_letter = input('Try to guess the letter i\'m thinking of:\n')
+        com_lett(my_letter)
 
 
 if __name__ == '__main__':
