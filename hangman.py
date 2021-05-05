a5b5b8ca4d8c932116307af3508baeb86ba6feb3
Sergio Enriquez Nava <eqz.nava@gmail.com>
2021-05-05 15:18:22 -0500

    Build the update function

--- hangman.py
+++ hangman.py
@@ -39,7 +39,6 @@ def transform_characters(lts=[]):
 
 def com_lett(my_letter):
     word_to_list = []
-    #indices = []
     words = []
     with open('C:/Users/natif/Escritorio/papa/Python/python_intermedio/proyecto_final/archivos/random_word.txt','r', encoding='utf-8') as file:
         for line in file:
@@ -56,23 +55,32 @@ def com_lett(my_letter):
             for valueW in indices:
                 if indexWTL == valueW:
                     w[valueW] = found_letters[0]
-        #Ahora hay que almacenar la lista modificada "w" en el archivo rw_transform.txt.
-        print(w)
-        print('\n')
+    return w
+
+
+def update(w):
+    with open('C:/Users/natif/Escritorio/papa/Python/python_intermedio/proyecto_final/archivos/rw_transform.txt','w', encoding='utf-8') as file:
+        a = ''.join(w)
+        file.write(a)
+        
+
+def show_update():
+    update_guess = []
+    with open('C:/Users/natif/Escritorio/papa/Python/python_intermedio/proyecto_final/archivos/rw_transform.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            update_guess = line
+        for lett in update_guess:
+            print(lett, end=' ')
+
     
-
-#Esta pendiente 
-def run():    
-    # my_list=[1,2,3]
-
-    # while not my_list:
-        print(read())
-        print('\n')
-        transform_characters(transform_to_list())
-        print('\n')
-        my_letter = input('Try to guess the letter i\'m thinking of: \n')
-        com_lett(my_letter)
+def main():    
+    print(read())
+    print('\n')
+    transform_characters(show_update())
+    print('\n')
+    my_letter = input('Try to guess the letter i\'m thinking of: \n')
+    update(com_lett(my_letter)
 
 
 if __name__ == '__main__':
-    run()
+    main()
